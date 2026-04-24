9f4d1ccd18dbae8e8d39392bec608ab9a7cc15a8
rlaghwjd2402-ctrl <gong2402@naver.com>
2026-04-24 13:28:59 +0900

    B 기능 개발 완료

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -22,8 +22,8 @@ public class Demo1Application {
             return "Application is running";
         }
 
-    public String getC() {
-        return "C";
+    public String getB() {
+        return "B";
     }
 
 }
