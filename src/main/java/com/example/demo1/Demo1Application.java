fe250e612948e197a8f3377017d0bd7d2f70a49d
rlaghwjd2402-ctrl <gong2402@naver.com>
2026-04-24 13:28:30 +0900

    C 기능 개발 완료

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -22,11 +22,8 @@ public class Demo1Application {
             return "Application is running";
         }
 
-    public String getStatus1() {
-        return "Application is running1";
-    }
-    public String getA() {
-        return "A";
+    public String getC() {
+        return "C";
     }
 
 }
